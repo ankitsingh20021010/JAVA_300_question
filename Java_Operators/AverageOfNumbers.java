55f78877fe6533ffb101c6dedb8d9a149680b084
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-12 19:50:27 +0530

    Add AverageOfNumbers class to calculate average

---
+++ Java_Operators/AverageOfNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AverageOfNumbers{
+ public static void main(String[] args){
+
+   Scanner input=new Scanner(System.in);
+   System.out.println("enter first number: ");
+   int num1=input.nextInt();
+
+    System.out.println("enter Second number: ");
+   int num2=input.nextInt();
+
+    System.out.println("Enter third number: ");
+   int num3=input.nextInt();
+
+   System.out.println("Average=" +(num1 + num2+ num3)/3);
+ }
+
+}
+
