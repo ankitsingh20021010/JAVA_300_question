bcbc6c5ea24400eb7c077942d7dbb9d3afcc44a6
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-13 10:26:37 +0530

    Add PostIncrementOperator class with main method

---
+++ Java_basic_programs/PostIncrementOperator.java
@@ -0,0 +1,21 @@
+public class PostIncrementOperato{
+  public static void main(String[] args){
+    int a=5; 
+  int b=a++;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
+
+
+
+
+
+//--------
+//       |
+//       |
+//       |
+//       V
+   //output
+  //5
+  //6
