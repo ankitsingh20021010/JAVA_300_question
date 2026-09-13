a35a3cb9579e19c0cd98cefab39a87ff1ff057af
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-13 21:02:45 +0530

    Add SquareOfNumber.java to calculate square of a number

---
+++ Java_Operators/SqureOfNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SquareOfNumber{
+  public static void main(String[] args){
+ Scanner input=new Scanner(System.in);
+    System.out.print("Enter a number- ");
+    int num=input.nextInt();
+    System.out.println("Squre of number = " + (num * num));
+
+  }
+}
+    
