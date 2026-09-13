6e477c3ca3b0da788c31aeaf4a0f5badab44698d
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-13 09:56:03 +0530

    Add Hello_World.java to print 'Hello' and 'World'

---
+++ Java_basic_programs/Hello_World.java
@@ -0,0 +1,8 @@
+//WAP to print "hello" in the 1st line and "World" in the 2nd line
+
+public class Hello_World{
+  public static void main(String args[]){
+    System.out.println("Hello");
+    System.out.println("World");
+  }
+}
