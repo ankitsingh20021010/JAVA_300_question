9946e1a058140f70626b97ca8036bbabca4ce8a1
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-13 10:23:25 +0530

    Create PreIncrementOperator.java
    Add a Java program demonstrating pre-increment operator.

---
+++ Java_basic_programs/PreIncrementOperator.java
@@ -0,0 +1,12 @@
+public class PreIncrementOperator{
+  public static void main(String args[]){
+       int a=5;
+    int b= ++a;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
+
+//output 
+//6 
+//6 
