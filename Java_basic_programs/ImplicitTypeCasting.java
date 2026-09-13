746f4c50b5427b936ad2dfdf0d8bc5582293d5be
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-13 10:13:20 +0530

    Add ImplicitTypeCasting example in Java

---
+++ Java_basic_programs/ImplicitTypeCasting.java
@@ -0,0 +1,8 @@
+public class ImplicitTypeCasting{
+    public static void main(String[] args){
+      int a=10;
+      double d=a; //implicit type casting
+  System.out.println(d);
+  System.out.println(a);
+    }
+}
