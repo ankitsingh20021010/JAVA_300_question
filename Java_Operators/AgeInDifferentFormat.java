9105ecddbd02d1a38ffdc2355fd261dc4d70c3bc
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-13 21:44:36 +0530

    Implement age conversion to different formats

---
+++ Java_Operators/AgeInDifferentFormat.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AgeInDifferentFormat {
+  public static void main(String [] args){
+    Scanner input= new Scanner(System.in);
+
+  System.out.println("Enter your age in years");
+    int age =input.nextInt();
+
+System.out.println("Your age: " + age + " years");
+System.out.println("Your age: " + age * 12 + " months");
+System.out.println("Your age: " + age * 365 + " days");
+System.out.println("Your age " + age * 365 * 24 + " hours old");
+System.out.println("Your age: " + age * 365 * 24 * 60 + " minutes");
+  }
+}
