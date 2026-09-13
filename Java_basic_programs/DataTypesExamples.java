a4289572bb336270cbea769955b20d3650dcb8e3
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-13 10:10:34 +0530

    Fix boolean variable typo in DataTypesExamples.java

---
+++ Java_basic_programs/DataTypesExamples.java
@@ -0,0 +1,30 @@
+public class DataTypesExamples{
+     public static void main(String[] args){
+    //Number(byte:1, short:2, int:4, long:8, float, double)
+   //Sentence(String)
+   //Characters(Char)
+  //True or False(bollean)
+
+  byte byteVariable=127;
+  short shortVariable=128;
+  int intVariable=500;
+  long longVariable=1032423L;
+  float floatVariable=1.5f;
+  double doubleVariable=5464564.5;
+  boolean booleanVariable=true;
+
+       
+  //Character value needs to be surround with single quotes
+  char charVariable='a';
+  String stringVariable="This is java String Statement";
+  System.out.println(byteVariable);
+  System.out.println(shortVariable);
+  System.out.println(intVariable);
+  System.out.println(longVariable);
+  System.out.println(floatVariable);
+  System.out.println(doubleVariable);
+  System.out.println(booleanVariable);
+  System.out.println(charVariable);
+  System.out.println(stringVariable);
+     }
+}
