3a8020aa19c857cd98ccf2dcc9b2915ad93aeb4c
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-13 10:32:16 +0530

    Create PrecedenceLogicOperator class
    Add PrecedenceLogicOperator class to demonstrate logic operator precedence.

---
+++ Java_basic_programs/PrecedenceLogicOperator.java
@@ -0,0 +1,12 @@
+public class PrecedenceLogicOperator{
+    public static void main(String [] args){
+      boolean b1=true,b2=false, b3=true;
+      boolean result=b1 && b2 || b3 && b2 || b2|| b3 && b1;
+      System.out.println(result);
+    }
+}
+
+
+
+//output
+//true
