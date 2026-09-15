ce8b680b62520720b3cae4cca6bd6138fb1916d0
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-15 08:09:58 +0530

    Implement variable swapping in SwapTwoVariables2
    Fix syntax errors in the SwapTwoVariables2 class.

---
+++ Java_Operators/SwapTwoVariables2.java
@@ -0,0 +1,12 @@
+public class SwapTwoVariables2{
+      puboic static void main(String [] args){
+      int num1=10, num2=100; 
+        num1=num1*num2;
+        num2=num1/num2;
+        num1=num1/num2;
+
+  System.out.println({"Values after Swapping");
+        System.out.println(num1);
+        System.out.println(num2);
+      }
+}
