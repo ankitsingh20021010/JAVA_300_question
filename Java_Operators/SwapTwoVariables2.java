94c6cdbfd6f68ec81e46c6f1c57ea2602adeae35
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-15 08:11:53 +0530

    Fix typos in SwapTwoVariables2.java

--- Java_Operators/SwapTwoVariables2.java
+++ Java_Operators/SwapTwoVariables2.java
@@ -1,11 +1,11 @@
 public class SwapTwoVariables2{
-      puboic static void main(String [] args){
+      public static void main(String [] args){
       int num1=10, num2=100; 
         num1=num1*num2;
         num2=num1/num2;
         num1=num1/num2;
 
-  System.out.println({"Values after Swapping");
+  System.out.println("Values after Swapping");
         System.out.println(num1);
         System.out.println(num2);
       }
