b5e864dd345783e0b63efabb2d12e6d809fb3d7a
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-16 11:25:32 +0530

    Fix syntax errors and add division check by 5

--- Conditional_statements/CheckNumberDivisionBy5.java
+++ Conditional_statements/CheckNumberDivisionBy5.java
@@ -1,6 +1,17 @@
-pubic class CheckNumberDivisionBy5{
+import java.util.Scanner;
+public class CheckNumberDivisionBy5{
    public static void main(String[] args){
-       
-   }
+    Scanner input=new Scanner(System.in);
+      int num;
+      System.out.print("Enter any Number");
+      num=input.nextln();
 
+      if(num%5==0){
+         System.out.println(num + " is divisible by 5");
+      }
+      else{
+         System.out.println(num + "is not divisible by 5");
+      }
+      
+   }
 }
