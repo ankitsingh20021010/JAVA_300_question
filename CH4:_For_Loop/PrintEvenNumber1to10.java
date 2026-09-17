36139347acd02d15306f8cb62d5dab2b667fc7f1
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-17 12:42:38 +0530

    Update and rename PrintEvenNumber1to10.java to PrintEvenNumber1to100.java

--- CH4:_For_Loop/PrintEvenNumber1to10.java
+++
@@ -1,7 +0,0 @@
-//WAP to print Even number 1 to n number (10,100)
-import java.util.Scanner;
-public class PrintEvenNumber1to10{
-  public static void main(String[] agrs){
-  
-  }
-}
---
+++ CH4:_For_Loop/PrintEvenNumber1to100.java
@@ -0,0 +1,10 @@
+//WAP to print Even number 1 to n number (10,100)
+public class PrintEvenNumber1to100{
+    public static void main(String [] args){
+        for(int i=2; i<=100; i++){
+            if(i%2==0){
+                System.out.println(i +" even");
+            }
+        }
+    }
+}
