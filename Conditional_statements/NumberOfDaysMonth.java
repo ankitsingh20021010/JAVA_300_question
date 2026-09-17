08a3e2c76269cbc33e4f6376dcf68efd30607501
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-17 12:24:50 +0530

    Add NumberOfDaysMonth.java to check days in month
    Implement a program to display the number of days in a given month using if-else statements.

---
+++ Conditional_statements/NumberOfDaysMonth.java
@@ -0,0 +1,47 @@
+//WAP to print number of days in given month using If-Else 
+import java.util.Scanner;
+public class NumberOfDaysMonth{
+  public static void main(String [] args){
+  Scanner input=new Scanner(System.in);
+    System.out.print("Enter a month number: ");
+    int month=input.nextInt();
+    if(month==1){
+     System.out.println(" 31 Days");
+    }
+     else if(month==2){
+     System.out.println("28/29 Days");
+    }
+       else if(month==3){
+     System.out.println(" 31 Days");
+    }
+     else if(month==4){
+     System.out.println("30 Days");
+    }
+       else if(month==5){
+     System.out.println("31 Days");
+    }
+       else if(month==6){
+     System.out.println("30 Days");
+    }   else if(month==7){
+     System.out.println("31 Days");
+    }
+       else if(month==8){
+     System.out.println("31 Days");
+    }
+       else if(month==9){
+     System.out.println("30 Days");
+    }
+       else if(month==10){
+     System.out.println("31 Days");
+    }
+       else if(month==11){
+     System.out.println("30 Days");
+    }
+       else if(month==12){
+     System.out.println("31 Days");
+    }
+    else{
+    System.out.println("Please enter number from 1 to 12 only");
+    }
+  }
+}
