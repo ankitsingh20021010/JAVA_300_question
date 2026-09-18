6f5c20a9e6bf57cb130c1263f4c230650460dc93
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-18 12:34:45 +0530

    Add pattern printing program in Pattern.java
    This program prints a pattern of numbers where each line contains the numbers 1 to 5.

---
+++ CH7:_Nested_Loop/Pattern.java
@@ -0,0 +1,13 @@
+//WAP to print --> 12345 next 5 line same number 12345
+
+public class Pattern{
+public static void main(String[]args){
+  int NumofRows=5;
+  for(int i=1; i<=NumofRows; i++){
+  for(int j=1; j<=NumofRows; j++){
+    System.out.print(j);
+  }
+    System.out.println();
+  }
+}
+}
