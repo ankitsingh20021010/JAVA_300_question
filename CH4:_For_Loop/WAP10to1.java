da6a7e7af154328514074fe608d5cad231df8a17
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-18 08:13:14 +0530

    Add WAP10to1.java to print numbers 10 to 1
    Implement a program to print numbers from 10 to 1 in reverse order.

---
+++ CH4:_For_Loop/WAP10to1.java
@@ -0,0 +1,9 @@
+//WAP to print 10 to 1 reverce order number 
+public class WAP10to1{
+ public static void main(String[] args){
+   for(int i=10; i>=1; i--){
+     System.out.println(i);
+   }
+ }
+}
+//output 10 9  8 7 6 5 4 3 2 1
