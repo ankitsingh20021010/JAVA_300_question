d4c678e76d327dbd3fc29e3b1e7610414d81b78f
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-18 12:37:52 +0530

    Add Pattern2 class to print number patterns
    Implemented a nested loop to print rows of numbers.

---
+++ CH7:_Nested_Loop/Pattern2.java
@@ -0,0 +1,12 @@
+//WAP to print 11111 and 22222 /n  33333/ 44444 /55555
+public class Pattern2{
+  public static void main(String[] args){
+    int NumofRows=5;
+    for(int i=1; i<=NumofRows; i++){
+      for(int j=1; j<=NumofRows; j++){
+        System.out.print(i);
+      }
+      System.out.println();
+    }
+  }
+}
