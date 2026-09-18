73a2a777406fe3e748b48ca90693d411cb932a41
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-18 09:09:16 +0530

    Add Print10To1.java to print numbers 10 to 1

---
+++ CH:05_While_Loop/Print10To1.java
@@ -0,0 +1,11 @@
+//using while loop print 10 9 8 7 6 ----1
+
+public class main{
+  public static void main(String[] args){
+ int i=10;
+    while(i>=1){
+      System.out.println(i);
+      i--;
+  }
+}
+}
