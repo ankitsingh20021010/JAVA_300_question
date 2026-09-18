a150ca4e595cc375d5cff75c49229613e469d7fa
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-18 08:40:52 +0530

    Add PrintZigZag class to print zigzag pattern

---
+++ CH4:_For_Loop/PrintZigZag.java
@@ -0,0 +1,10 @@
+//1 10 2 9 3 8 4 7 5 6
+public class PrintZigZag{
+public static void main(String[] args){
+
+  for(int i=1; i<=5; i++){
+  System.out.print(i +" ");
+  System.out.print(11-i + " " );
+  }
+}
+}
