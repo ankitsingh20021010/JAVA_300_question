acc25bea76da9ee2cf87311fe700d033e7a40de3
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-19 19:14:45 +0530

    Rename class from 'pattern' to 'Pattern8'

--- CH7:_Nested_Loop/Pattern8.java
+++ CH7:_Nested_Loop/Pattern8.java
@@ -1,5 +1,5 @@
 //WAP to print start 5*5
-public class pattern{
+public class Pattern8{
     public static void main(String[] args){
         int numofRows=5;
         for(int i=1; i<=numofRows; i++){
