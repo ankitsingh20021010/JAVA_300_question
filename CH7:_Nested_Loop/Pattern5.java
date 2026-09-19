1d45f7858588f1f73d1c0b53bd0e37b261a07742
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-19 19:02:30 +0530

    Add Pattern5.java to print descending number pattern
    Implement a program to print a pattern of numbers in descending order.

---
+++ CH7:_Nested_Loop/Pattern5.java
@@ -0,0 +1,13 @@
+//WAP 55555 /n 44444 /n 33333 /n /22222 /n 111111
+
+public class pattern5{
+    public static void main(String[] args){
+        int numOfRows=5;
+        for(int i=numOfRows; i>=1; i--){
+            for(int j=1; j<=5; j++){
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+    }
+}
