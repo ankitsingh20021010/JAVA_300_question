9cbf34ef0df791199014f85f436a3a23b702ffb9
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-19 19:13:24 +0530

    Add Pattern8.java to print a 5x5 star pattern

---
+++ CH7:_Nested_Loop/Pattern8.java
@@ -0,0 +1,13 @@
+//WAP to print start 5*5
+public class pattern{
+    public static void main(String[] args){
+        int numofRows=5;
+        for(int i=1; i<=numofRows; i++){
+            for(int j=1; j<=5; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+     
