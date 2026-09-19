25798cf00eea72e915a2235c8eee78c2a89a04a3
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-19 19:19:29 +0530

    Fix class name and implement character printing pattern
    Corrected class name and adjusted loop logic to print characters correctly.

--- CH7:_Nested_Loop/Pattern9.java
+++ CH7:_Nested_Loop/Pattern9.java
@@ -1,1 +1,14 @@
-
+//WAP to print AAAAA /n B B B B B /n C C C C C /n D D D D D /n  E E E E E /n
+public class Patterm8{
+  public static void main(String[] args){
+      int NumOfRows=5;
+      char ch='A';
+      for(int i=1; i<=NumOfRows; i++){
+          for(int j=1; j<=NumOfRows; j++){
+              System.out.print(ch + " ");
+          }
+          ch=(char)(ch+1);
+          System.out.println("");
+      }
+  }
+ }
