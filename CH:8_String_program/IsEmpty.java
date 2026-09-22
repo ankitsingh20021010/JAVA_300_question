d4630744a87eb966713e6b348d5789b3d5d1536e
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-22 08:20:40 +0530

    Add IsEmpty.java to demonstrate isEmpty() method
    Demonstrate the usage of isEmpty() method on strings.

---
+++ CH:8_String_program/IsEmpty.java
@@ -0,0 +1,11 @@
+//how to work isEmpty(); give true flase (boolen value)
+public class IsEmpty{
+  public static void main(String[] args){
+    String str1="Hello, Welcome to java Classes";
+    String str2=" ";
+    String str3="";
+    System.out.println(str1.isEmpty()); //false
+    System.out.println(str2.isEmpty()); //false
+    System.out.println(str3.isEmpty()); //true
+  }
+}
