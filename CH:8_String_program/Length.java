314acfb9415e3d965ea1767536bfc8fdf71acb15
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-09-22 08:25:16 +0530

    Add Length.java to print string lengths
    This program prints the lengths of two strings.

---
+++ CH:8_String_program/Length.java
@@ -0,0 +1,10 @@
+//WAP to print length of string 
+public class Length{
+  public static void main(String[] args){
+    String str1="Hello, Welcome to java classes";
+    String str2="hello";
+    String str3="Hello";
+    System.out.println("Length of Str1 is: "+str1.length());
+    System.out.println("Length of Str2  is: "+str2.length());
+  }
+}
